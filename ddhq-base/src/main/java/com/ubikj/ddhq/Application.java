54eff2575bfe0155f084e09fd3adc63eacedf0ee
mlkvocano <1151355221@qq.com>
2018-06-05 20:57:59 +0800

    create

---
+++ ddhq-base/src/main/java/com/ubikj/ddhq/Application.java
@@ -0,0 +1,16 @@
+package com.ubikj.ddhq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+        System.out.println("22222");
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
---
+++ ddhq-web/src/main/java/com/ubikj/ddhq/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.ubikj.ddhq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(ModelMap map){
+        map.addAttribute("host","www.jianshu.com");
+        return "index";
+    }
+}
