ed02a38efc679736754a6191d51cad6127647e4f
mlkvocano <1151355221@qq.com>
2018-06-05 20:15:20 +0800

    create

--- ddhq-web/src/main/java/com/ubikj/ddhq/web/Application.java
+++ ddhq-web/src/main/java/com/ubikj/ddhq/web/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("111111111111");
+        System.out.println("22222");
         SpringApplication.run(Application.class, args);
     }
 
